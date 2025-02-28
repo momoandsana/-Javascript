e771e7df4d82ebc616b23b336ef6cffe5e2965db
hoseok <swiftie52@naver.com>
2025-02-28 10:07:03 +0900

    fix : 불필요한 파일 삭제

--- ch01/03-08/03-08.js
+++
@@ -1,5 +0,0 @@
-const season = ['봄', '여름', '가을', '겨울'];
-
-season.forEach((value) => {
-    console.log(`${value}`);
-})
--- ch01/03-10/03-10.js
+++
@@ -1,9 +0,0 @@
-const suspect = ['hoya', 'judy', 'Tom', 'Joy'];
-
-const criminal = 'hoya';
-
-const usual_suspect = suspect.filter((value) => {
-    return value == criminal;
-})
-
-alert(`용의자 ${suspect} 중 범인은 ${usual_suspect}입니다.`);
